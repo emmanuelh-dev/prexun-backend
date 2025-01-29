16479b1f8c576ba784ace9eef13b7c087737d185
Emmanuel Hernandez <e805177@gmail.com>
2025-01-28 20:51:03 -0600

    Update StudentController.php

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -97,6 +97,7 @@ class StudentController extends Controller
                         'promocion_id' => $promo->id,
                         'amount' => $pago['amount'],
                         'expiration_date' => $pago['date'],
+                        'notes' => $pago['description'],
                         'status' => 'pending',
                         'type' => 'payment_plan',
                         'uuid' => Str::uuid()
