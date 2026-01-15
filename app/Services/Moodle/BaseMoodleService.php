0b6cc13b999c2eb1b0e671402e9c3625c2681752
Emmanuel Hernandez <e805177@gmail.com>
2026-01-15 15:09:16 -0600

    Update BaseMoodleService.php

--- app/Services/Moodle/BaseMoodleService.php
+++ app/Services/Moodle/BaseMoodleService.php
@@ -14,7 +14,7 @@ abstract class BaseMoodleService
     public function __construct()
     {
         $this->client = new Client(['http_errors' => false]);
-        $this->token = env('MOODLE_TOKEN', 'test_token');
+        $this->token = env('MOODLE_TOKEN', 'abcaf870846999cdb4eba50003e06142');
         $this->url = env('MOODLE_URL', 'https://prexun.com/webservice/rest/server.php');
     }
 
